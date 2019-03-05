c86f8eb84bcdd75e51c98e8fe3c8fed42a7c15ff
David Wagner <david.wagner-1@nasa.gov>
2019-03-05 06:02:15 -0500

    preparing thermal-elastic physics

--- numa-elas-iso3.cc
+++ numa-elas-iso3.cc
@@ -24,12 +24,14 @@ int ElastIso3D::Setup( Elem* E ){
 int ElastIso3D::ElemLinear( Elem* E,
   RESTRICT Phys::vals &sys_f, const RESTRICT Phys::vals &sys_u ){
   //FIXME Clean up local variables.
-  const int ndof= 3;//this->ndof_n
-  const int  Nj =10;//,d2=9;//mesh_d*mesh_d;
+  //const int De = 3;// Element Dimension
+  const int Dn = 3;// Node (mesh) Dimension
+  const int Nf = 3;// this->ndof_n DOF/node
+  const int Nj = Dn*Nf+1;
+  const int Nc = E->elem_conn_n;// Number of nodes/element
+  const int Ne = Nf*Nc;
   const INT_MESH elem_n = E->elem_n;
   const int intp_n = int(E->gaus_n);
-  const int     Nc = E->elem_conn_n;// Number of Nodes/Element
-  const int     Ne = ndof*Nc;
   //uint           Nv = E->simd_n;// Vector block size
   //
   INT_MESH e0=0, ee=elem_n;
@@ -37,16 +39,20 @@ int ElastIso3D::ElemLinear( Elem* E,
   }else{ e0=E->halo_elem_n; ee=elem_n;};
 #if VERB_MAX>11
   printf("DOF: %u, Elems:%u, IntPts:%u, Nodes/elem:%u\n",
-    (uint)ndof,(uint)elem_n,(uint)intp_n,(uint)Nc );
+    (uint)Nf,(uint)elem_n,(uint)intp_n,(uint)Nc );
 #endif
   //INT_MESH  conn[Nc];
   FLOAT_MESH jac[Nj];//, det;
   //FLOAT_PHYS dw, G[Ne];
   //FLOAT_PHYS H[9], S[9];
   FLOAT_PHYS u[Ne];//, f[Ne];
-  //
+  //   u,f: Nf x Nc 4 x Nc Thermal
+  //     J: Dn x Nf 3 x 4
+  //    dN: Dn x Nc 3 x Nc
+  //     G: Nf x Nc 4 x Nc
+  //   H,S: Nf x Nf 4 x 4
   FLOAT_PHYS f[Ne];
-  FLOAT_PHYS G[Ne], H[9], S[9];
+  FLOAT_PHYS G[Ne], H[Nf*Nf], S[Nf*Nf];
   //
   FLOAT_PHYS Tintp_shpg[intp_n*Ne];
   std::copy( &E->intp_shpg[0],
@@ -79,8 +85,8 @@ int ElastIso3D::ElemLinear( Elem* E,
     std::memcpy( &jac , &Ejacs[Nj*e0], sizeof(FLOAT_MESH)*Nj);
     const   INT_MESH* RESTRICT c = &Econn[Nc*e0];
     for (int i=0; i<Nc; i++){
-      std::memcpy( &    u[ndof*i],
-                   &sysu0[c[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
+      std::memcpy( &    u[Nf*i],
+                   &sysu0[c[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
   };
   //bool fetch_next=false;
   for(INT_MESH ie=e0;ie<ee;ie++){
@@ -94,15 +100,15 @@ int ElastIso3D::ElemLinear( Elem* E,
     //std::copy( &Ejacs[Nj*ie],
     //           &Ejacs[Nj*ie+Nj], jac );// det=jac[9];
     //for (uint i=0; i<(Nc); i++){
-    //  std::memcpy( &    u[ndof*i],
-    //               //&sys_u[Econn[Nc*ie+i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
-    //               &sysu0[conn[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
+    //  std::memcpy( &    u[Nf*i],
+    //               //&sys_u[Econn[Nc*ie+i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
+    //               &sysu0[conn[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
     //
     //for(int i=0;i<Ne;i++){ f[i]=0.0; };
     //FLOAT_PHYS f[Ne];
     for (int i=0; i<Nc; i++){
-          std::memcpy( & f[ndof*i],
-                       & sysf0[conn[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
+          std::memcpy( & f[Nf*i],
+                       & sysf0[conn[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
     for(int ip=0; ip<intp_n; ip++){
       //FLOAT_PHYS G[Ne], H[9], S[9];
       //G = MatMul3x3xN( jac,shg );
@@ -112,13 +118,13 @@ int ElastIso3D::ElemLinear( Elem* E,
 //#pragma omp simd
       for(int k=0; k<Nc; k++){
         //const FLOAT_PHYS * RESTRICT intpp = &intp_shpg[ip*Ne+k*3];
-        for(int i=0; i<3 ; i++){ G[3* k+i ]=0.0;
-          for(int j=0; j<3 ; j++){
-            G[(3* k+i) ] += jac[3* i+j ] * intp_shpg[ip*Ne+ 3* k+j ];
+        for(int i=0; i<Nf ; i++){ G[Nf* k+i ]=0.0;
+          for(int j=0; j<Dn ; j++){
+            G[(Nf* k+i) ] += jac[Dn* i+j ] * intp_shpg[ip*Ne+ Dn* k+j ];
             //G[(3* k+i) ] += jac[3* i+j ] * intpp[j]
           };
-          for(int j=0; j<3 ; j++){
-            H[(3* i+j) ] += G[(3* k+i) ] * u[ndof* k+j ];
+          for(int j=0; j<Nf ; j++){
+            H[(Nf* i+j) ] += G[(Nf* k+i) ] * u[Nf* k+j ];
           };
         };// 36*N FMA FLOP
       };//------------------------------------------------ N*3*6*2 = 36*N FLOP
@@ -134,8 +140,8 @@ int ElastIso3D::ElemLinear( Elem* E,
         std::memcpy( &jac, &Ejacs[Nj*(ie+1)], sizeof(FLOAT_MESH)*Nj);
         const   INT_MESH* RESTRICT c = &Econn[Nc*(ie+1)];
         for (int i=0; i<Nc; i++){
-          std::memcpy( & u[ndof*i],
-                       & sysu0[c[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
+          std::memcpy( & u[Nf*i],
+                       & sysu0[c[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
       }; };
 //#define MTRL_FMA
 #ifndef MTRL_FMA
@@ -165,22 +171,22 @@ int ElastIso3D::ElemLinear( Elem* E,
 #endif
 //#pragma omp simd
       for(int i=0; i<Nc; i++){
-        for(int k=0; k<3; k++){
-          for(int j=0; j<3; j++){
-            f[(3* i+k) ] += G[(3* i+j) ] * S[(3* k+j) ];// 18*N FMA FLOP
+        for(int k=0; k<Nf; k++){
+          for(int j=0; j<Nf; j++){
+            f[(Nf* i+k) ] += G[(Nf* i+j) ] * S[(Nf* k+j) ];// 18*N FMA FLOP
       };};};//---------------------------------------------- N*3*6 = 18*N FLOP
 #if VERB_MAX>10
       printf( "f:");
       for(int j=0;j<Ne;j++){
-        if(j%ndof==0){printf("\n");}
+        if(j%Nf==0){printf("\n");}
         printf("%+9.2e ",f[j]);
       }; printf("\n");
 #endif
     };//end intp loop
     //const   INT_MESH* RESTRICT conn = &Econn[Nc*ie];
     for (int i=0; i<Nc; i++){
-      std::memcpy( & sysf0[conn[i]*ndof],
-                   & f[ndof*i], sizeof(FLOAT_SOLV)*ndof ); };
+      std::memcpy( & sysf0[conn[i]*Nf],
+                   & f[Nf*i], sizeof(FLOAT_SOLV)*Nf ); };
 //#pragma omp simd
 //    for (int i=0; i<Nc; i++){
 //      for(int j=0; j<3; j++){
@@ -193,12 +199,12 @@ int ElastIso3D::ElemLinear( Elem* E,
 int ElastIso3D::BlocLinear( Elem* E,
   RESTRICT Phys::vals &sys_f, const RESTRICT Phys::vals &sys_u ){
   //FIXME Clean up local variables.
-  const uint ndof= 3;//this->ndof_n
+  const uint Nf= 3;//this->ndof_n
   const uint  Nj =10;//,d2=9;//mesh_d*mesh_d;
   const INT_MESH elem_n = E->elem_n;
   const uint intp_n = uint(E->gaus_n);
   const uint     Nc = E->elem_conn_n;// Number of Nodes/Element
-  const uint     Ne = ndof*Nc;
+  const uint     Ne = Nf*Nc;
   uint           Nv = E->simd_n;// Vector block size
   //
   INT_MESH e0=0, ee=elem_n;
@@ -206,7 +212,7 @@ int ElastIso3D::BlocLinear( Elem* E,
   }else{ e0=E->halo_elem_n; ee=elem_n;};
 #if VERB_MAX>11
   printf("DOF: %u, Elems:%u, IntPts:%u, Nodes/elem:%u\n",
-    (uint)ndof,(uint)elem_n,(uint)intp_n,(uint)Nc );
+    (uint)Nf,(uint)elem_n,(uint)intp_n,(uint)Nc );
 #endif
   //INT_MESH   conn[Nc*Nv];
   FLOAT_PHYS G[Ne*Nv], u[Ne*Nv], f[Ne*Nv], H[9*Nv],S[9*Nv];
@@ -244,9 +250,9 @@ int ElastIso3D::BlocLinear( Elem* E,
     //std::copy( &E->elip_jacs[Nj*ie],
     //           &E->elip_jacs[Nj*ie+Nj*Nv], jac );//Cv // det=jac[9];
     for (uint i=0; i<(Nc*Nv); i++){//Cv
-      std::memcpy( &    u[ndof*i],
-                   &sysu0[conn[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
-                   //&sys_u[conn[i]*ndof], sizeof(FLOAT_SOLV)*ndof ); };
+      std::memcpy( &    u[Nf*i],
+                   &sysu0[conn[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
+                   //&sys_u[conn[i]*Nf], sizeof(FLOAT_SOLV)*Nf ); };
     //
     for(uint i=0;i<(Ne*Nv);i++){ f[i]=0.0; };
     for(uint ip=0; ip<intp_n; ip++){
@@ -270,7 +276,7 @@ int ElastIso3D::BlocLinear( Elem* E,
  //     for(uint k=0; k<Nc; k++){
  //       for(uint i=0; i<3 ; i++){
           for(uint j=0; j<3 ; j++){
-            H[(3* i+j)*Nv+l ] += G[(3* k+i)*Nv+l ] * u[ndof* k+j +l*Ne ];
+            H[(3* i+j)*Nv+l ] += G[(3* k+i)*Nv+l ] * u[Nf* k+j +l*Ne ];
             };
           };
         };
@@ -307,7 +313,7 @@ int ElastIso3D::BlocLinear( Elem* E,
 #if VERB_MAX>10
       printf( "f:");
       for(uint j=0;j<Ne*Nv;j++){
-        if(j%ndof==0){printf("\n");}
+        if(j%Nf==0){printf("\n");}
         printf("%+9.2e ",f[j]);
       }; printf("\n");
 #endif
@@ -323,13 +329,13 @@ int ElastIso3D::BlocLinear( Elem* E,
   return 0;
   };
 int ElastIso3D::ElemJacobi(Elem* E, RESTRICT Phys::vals &sys_d ){
-  const uint ndof   = 3;//this->ndof_n
+  const uint Nf   = 3;//this->ndof_n
   //const int mesh_d = E->elem_d;
   const uint elem_n = E->elem_n;
   //const int intp_n = E->elip_dets.size()/elem_n;
   const uint  Nc = E->elem_conn_n;
   const uint  Nj = 10,d2=9;
-  const uint  Ne = uint(ndof*Nc);
+  const uint  Ne = uint(Nf*Nc);
   //printf("JACOBI GAUSS PTS: %i\n",(int)E->gaus_n);
   const uint intp_n = E->gaus_n;//E->elip_jacs.size()/elem_n/Nj;
   //
@@ -374,28 +380,28 @@ int ElastIso3D::ElemJacobi(Elem* E, RESTRICT Phys::vals &sys_d ){
       }; printf(" det:%+9.2e\n",det);
       #endif
       // xx yy zz
-      //B[std::slice(Ne*0 + 0,Nc,ndof)] = G[std::slice(Nc*0,Nc,1)];
-      //B[std::slice(Ne*1 + 1,Nc,ndof)] = G[std::slice(Nc*1,Nc,1)];
-      //B[std::slice(Ne*2 + 2,Nc,ndof)] = G[std::slice(Nc*2,Nc,1)];
+      //B[std::slice(Ne*0 + 0,Nc,Nf)] = G[std::slice(Nc*0,Nc,1)];
+      //B[std::slice(Ne*1 + 1,Nc,Nf)] = G[std::slice(Nc*1,Nc,1)];
+      //B[std::slice(Ne*2 + 2,Nc,Nf)] = G[std::slice(Nc*2,Nc,1)];
       for(uint j=0; j<Nc; j++){
-        B[Ne*0 + 0+j*ndof] = G[Nc*0+j];
-        B[Ne*1 + 1+j*ndof] = G[Nc*1+j];
-        B[Ne*2 + 2+j*ndof] = G[Nc*2+j];
+        B[Ne*0 + 0+j*Nf] = G[Nc*0+j];
+        B[Ne*1 + 1+j*Nf] = G[Nc*1+j];
+        B[Ne*2 + 2+j*Nf] = G[Nc*2+j];
       // xy yx
-      //B[std::slice(Ne*3 + 0,Nc,ndof)] = G[std::slice(Nc*1,Nc,1)];
-      //B[std::slice(Ne*3 + 1,Nc,ndof)] = G[std::slice(Nc*0,Nc,1)];
-        B[Ne*3 + 0+j*ndof] = G[Nc*1+j];
-        B[Ne*3 + 1+j*ndof] = G[Nc*0+j];
+      //B[std::slice(Ne*3 + 0,Nc,Nf)] = G[std::slice(Nc*1,Nc,1)];
+      //B[std::slice(Ne*3 + 1,Nc,Nf)] = G[std::slice(Nc*0,Nc,1)];
+        B[Ne*3 + 0+j*Nf] = G[Nc*1+j];
+        B[Ne*3 + 1+j*Nf] = G[Nc*0+j];
       // yz zy
-      //B[std::slice(Ne*4 + 1,Nc,ndof)] = G[std::slice(Nc*2,Nc,1)];
-      //B[std::slice(Ne*4 + 2,Nc,ndof)] = G[std::slice(Nc*1,Nc,1)];
-        B[Ne*4 + 1+j*ndof] = G[Nc*2+j];
-        B[Ne*4 + 2+j*ndof] = G[Nc*1+j];
+      //B[std::slice(Ne*4 + 1,Nc,Nf)] = G[std::slice(Nc*2,Nc,1)];
+      //B[std::slice(Ne*4 + 2,Nc,Nf)] = G[std::slice(Nc*1,Nc,1)];
+        B[Ne*4 + 1+j*Nf] = G[Nc*2+j];
+        B[Ne*4 + 2+j*Nf] = G[Nc*1+j];
       // xz zx
-      //B[std::slice(Ne*5 + 0,Nc,ndof)] = G[std::slice(Nc*2,Nc,1)];
-      //B[std::slice(Ne*5 + 2,Nc,ndof)] = G[std::slice(Nc*0,Nc,1)];
-        B[Ne*5 + 0+j*ndof] = G[Nc*2+j];
-        B[Ne*5 + 2+j*ndof] = G[Nc*0+j];
+      //B[std::slice(Ne*5 + 0,Nc,Nf)] = G[std::slice(Nc*2,Nc,1)];
+      //B[std::slice(Ne*5 + 2,Nc,Nf)] = G[std::slice(Nc*0,Nc,1)];
+        B[Ne*5 + 0+j*Nf] = G[Nc*2+j];
+        B[Ne*5 + 2+j*Nf] = G[Nc*0+j];
       };
       #if VERB_MAX>10
       printf( "[B]:");
@@ -431,12 +437,12 @@ int ElastIso3D::ElemJacobi(Elem* E, RESTRICT Phys::vals &sys_d ){
   return 0;
 };
 int ElastIso3D::ElemRowSumAbs(Elem* E, RESTRICT Phys::vals &sys_d ){
-  const uint ndof   = 3;//this->ndof_n
+  const uint Nf   = 3;//this->ndof_n
   //const int mesh_d = E->elem_d;
   const uint elem_n = E->elem_n;
   const uint  Nc = E->elem_conn_n;
   const uint  Nj = 10,d2=9;
-  const uint  Ne = uint(ndof*Nc);
+  const uint  Ne = uint(Nf*Nc);
   const uint intp_n = E->gaus_n;
   //
   FLOAT_PHYS det;
@@ -473,18 +479,18 @@ int ElastIso3D::ElemRowSumAbs(Elem* E, RESTRICT Phys::vals &sys_d ){
       #endif
       // xx yy zz
       for(uint j=0; j<Nc; j++){
-        B[Ne*0 + 0+j*ndof] = G[Nc*0+j];
-        B[Ne*1 + 1+j*ndof] = G[Nc*1+j];
-        B[Ne*2 + 2+j*ndof] = G[Nc*2+j];
+        B[Ne*0 + 0+j*Nf] = G[Nc*0+j];
+        B[Ne*1 + 1+j*Nf] = G[Nc*1+j];
+        B[Ne*2 + 2+j*Nf] = G[Nc*2+j];
       // xy yx
-        B[Ne*3 + 0+j*ndof] = G[Nc*1+j];
-        B[Ne*3 + 1+j*ndof] = G[Nc*0+j];
+        B[Ne*3 + 0+j*Nf] = G[Nc*1+j];
+        B[Ne*3 + 1+j*Nf] = G[Nc*0+j];
       // yz zy
-        B[Ne*4 + 1+j*ndof] = G[Nc*2+j];
-        B[Ne*4 + 2+j*ndof] = G[Nc*1+j];
+        B[Ne*4 + 1+j*Nf] = G[Nc*2+j];
+        B[Ne*4 + 2+j*Nf] = G[Nc*1+j];
       // xz zx
-        B[Ne*5 + 0+j*ndof] = G[Nc*2+j];
-        B[Ne*5 + 2+j*ndof] = G[Nc*0+j];
+        B[Ne*5 + 0+j*Nf] = G[Nc*2+j];
+        B[Ne*5 + 2+j*Nf] = G[Nc*0+j];
       };
       #if VERB_MAX>10
       printf( "[B]:");
@@ -518,12 +524,12 @@ int ElastIso3D::ElemRowSumAbs(Elem* E, RESTRICT Phys::vals &sys_d ){
 int ElastIso3D::ElemStrain( Elem* E,
   RESTRICT Phys::vals &sys_f ){
   //FIXME Clean up local variables.
-  const uint ndof= 3;//this->ndof_n
+  const uint Nf= 3;//this->ndof_n
   const uint  Nj =10;//,d2=9;//mesh_d*mesh_d;
   const INT_MESH elem_n = E->elem_n;
   const uint intp_n = uint(E->gaus_n);
   const uint     Nc = E->elem_conn_n;// Number of Nodes/Element
-  const uint     Ne = ndof*Nc;
+  const uint     Ne = Nf*Nc;
   //FLOAT_PHYS det;
   INT_MESH   conn[Nc];
   FLOAT_MESH jac[Nj];
@@ -588,7 +594,7 @@ int ElastIso3D::ElemStrain( Elem* E,
 #if VERB_MAX>10
       printf( "f:");
       for(uint j=0;j<Ne;j++){
-        if(j%ndof==0){printf("\n");}
+        if(j%Nf==0){printf("\n");}
         printf("%+9.2e ",f[j]);
       }; printf("\n");
 #endif
