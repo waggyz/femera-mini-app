f044392f9932834ada9014165b0423d9aa401a31
David Wagner <david.wagner-1@nasa.gov>
2019-06-11 11:29:38 -0400

    fixed KNL cpumodel bug

--- cpumodel.sh
+++ cpumodel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($7)}'`
 if [ "$CPUMODEL" == "CPU" ];then CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($6)}'`;fi
+if [ "$CPUMODEL" == "Phi(TM)" ];then CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($8)}'`;fi
 echo $CPUMODEL
