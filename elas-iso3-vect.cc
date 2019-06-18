cd2db891ed6ad5a1b709f2a15f77f2ec6d32d546
David Wagner <david.wagner-1@nasa.gov>
2019-06-18 08:50:00 -0400

    debugging elas-iso3-vec2.cc...

--- elas-iso3-vect.cc
+++ elas-iso3-vect.cc
@@ -119,21 +119,21 @@ int ElastIso3D::ElemLinear( Elem* E,
       __m256d is0,is1,is2,is3,is4,is5,is6,is7,is8;
       int ig=0;
       //for(int i=0; i<Ne; i+=6){
-      for(int i= 0; i<12; i+=9){// 3* 4
+      for(int i= 0; i<Ne; i+=9){// 3* 4
         is0= _mm256_set1_pd(isp[i+0]); is1= _mm256_set1_pd(isp[i+1]); is2= _mm256_set1_pd(isp[i+2]);
         u0 = _mm256_set1_pd(  u[i+0]); u1 = _mm256_set1_pd(  u[i+1]); u2 = _mm256_set1_pd(  u[i+2]);
         g0 = _mm256_add_pd(_mm256_mul_pd(j0,is0), _mm256_add_pd(_mm256_mul_pd(j1,is1),_mm256_mul_pd(j2,is2)));
         a036= _mm256_add_pd(a036, _mm256_mul_pd(g0,u0)); a147 = _mm256_add_pd(a147, _mm256_mul_pd(g0,u1)); a258 = _mm256_add_pd(a258, _mm256_mul_pd(g0,u2));
               _mm256_store_pd(&G[ig],g0);
         ig+=4;
-        if((i+3)<12){
+        if((i+5)<Ne){
         is3= _mm256_set1_pd(isp[i+3]); is4= _mm256_set1_pd(isp[i+4]); is5= _mm256_set1_pd(isp[i+5]);
         u3 = _mm256_set1_pd(  u[i+3]); u4 = _mm256_set1_pd(  u[i+4]); u5 = _mm256_set1_pd(  u[i+5]);
         g1 = _mm256_add_pd(_mm256_mul_pd(j0,is3), _mm256_add_pd(_mm256_mul_pd(j1,is4),_mm256_mul_pd(j2,is5)));
         a036= _mm256_add_pd(a036, _mm256_mul_pd(g1,u3)); a147 = _mm256_add_pd(a147, _mm256_mul_pd(g1,u4)); a258 = _mm256_add_pd(a258, _mm256_mul_pd(g1,u5));
               _mm256_store_pd(&G[ig],g1);
         ig+=4;
-        }if((i+6)<12){
+        }if((i+8)<Ne){
         is6= _mm256_set1_pd(isp[i+6]); is7= _mm256_set1_pd(isp[i+7]); is8= _mm256_set1_pd(isp[i+8]);
         u6 = _mm256_set1_pd(  u[i+6]); u7 = _mm256_set1_pd(  u[i+7]); u8 = _mm256_set1_pd(  u[i+8]);
         g2 = _mm256_add_pd(_mm256_mul_pd(j0,is6), _mm256_add_pd(_mm256_mul_pd(j1,is7),_mm256_mul_pd(j2,is8)));
@@ -142,6 +142,7 @@ int ElastIso3D::ElemLinear( Elem* E,
         ig+=4;
         }
       }
+#if 0
       if(elem_p>1){// 3* 10
       for(int i=12; i<30; i+=9){
         is0= _mm256_set1_pd(isp[i+0]); is1= _mm256_set1_pd(isp[i+1]); is2= _mm256_set1_pd(isp[i+2]);
@@ -182,6 +183,7 @@ int ElastIso3D::ElemLinear( Elem* E,
         a036= _mm256_add_pd(a036, _mm256_mul_pd(g2,u6)); a147 = _mm256_add_pd(a147, _mm256_mul_pd(g2,u7)); a258 = _mm256_add_pd(a258, _mm256_mul_pd(g2,u8));
               _mm256_store_pd(&G[i+8],g2);
       } }
+#endif
       _mm256_store_pd(&H[0],a036);
       _mm256_store_pd(&H[3+1],a147);
       _mm256_store_pd(&H[6+2],a258);
