8bbb6e3818269270d01ef05387872d39fc71b22b
David Wagner <david.wagner-1@nasa.gov>
2019-06-24 13:52:21 -0400

    Debugged makefile to count physical cores correctly.

--- cpucount.sh
+++ cpucount.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}'
+NCORE=`grep ^cpu\\\scores /proc/cpuinfo | uniq |  awk '{print($4)}'`
+NSOCK=`grep "physical id" /proc/cpuinfo | sort -u | wc -l`
+#echo $NCORE
+#echo $NSOCK
+NCPU=`expr $NCORE \* $NSOCK`
+echo $NCPU
