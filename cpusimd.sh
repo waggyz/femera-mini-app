afa500fd186730945d0586148b5a2ffcda245716
David Wagner <david.wagner-1@nasa.gov>
2019-09-04 12:23:30 -0400

    Debugging unit tests...

--- cpusimd.sh
+++ cpusimd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 ALLFLAGS=`grep -i -m1 flags /proc/cpuinfo | tr '[a-z]' '[A-Z]'`
-for FLAG in SSE SSE2 AVX AVX2 AVX-256 AVX-512 AVX512; do
+for FLAG in SSE SSE2 AVX AVX2 FMA AVX-256 AVX-512 AVX512; do
   if [[ $ALLFLAGS =~ $FLAG ]]; then
     MYFLAGS=$MYFLAGS" "$FLAG
   fi
