77adabda32c953bfa7de11b589a788c9ed21fa5e
David Wagner <david.wagner-1@nasa.gov>
2019-09-08 09:09:47 -0400

    Testing large size partitioning plot...

--- perf/profile-basic.sh
+++ perf/profile-basic.sh
@@ -198,7 +198,9 @@ if [ -n "$CSV_HAS_PART_TEST" ]; then
     title 'Performance at $MUDOF MDOF';"\
     | tee -a $PROFILE | grep --no-group-separator -C25 --color=always '\.'
   fi
-  MAX=`awk '{if(max<$13){max=$13;perf=$13/1e6;size=$1/$4}}END{print size perf  }' $CSVFILE`
+  MAX=`awk -F, -v max=0\
+    '($13>max)&&($4>$9){max=$13;perf=$13/1e6;size=$1/$4}END{print size,perf}'\
+    $CSVFILE`
   echo Maximum performance is $MAX
 fi
 #
