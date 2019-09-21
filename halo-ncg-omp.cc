38d43cc2fc01bfa84a96f6803c194cf3c1c277fd
David Wagner <david.wagner-1@nasa.gov>
2019-09-21 08:03:35 -0400

    tweaking nonlinear plasticity stuff...

--- halo-ncg-omp.cc
+++ halo-ncg-omp.cc
@@ -138,7 +138,14 @@ int HaloNCG::Init(){// printf("*** HaloNCG::Init() ***\n");
   auto m=bcmax[0];
   for(uint i=1;i<3;i++){ if(bcmax[0] > m){ m=bcmax[i]; } }
   for(uint i=0;i<3;i++){ bcmax[i]=m; }
+#if 1
+  //NOTE Nonlinear physics needs unscaled sys_u
+  //FIXME Check that sys_u is unscaled
   for(uint i=0;i<bcmax.size();i++){ if(bcmax[i]<=0.0){ bcmax[i]=1.0; } }
+#else
+  // Set all DOF scales to unity
+  for(uint i=0;i<bcmax.size();i++){ bcmax[i]=1.0; }
+#endif
 #if VERB_MAX>2
     if(verbosity>2){
       printf("    DOF Scales:");
