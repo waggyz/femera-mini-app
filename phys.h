71a4f0d98ee0d3c2331562421d8c118ee03c85c1
David Wagner <david.wagner-1@nasa.gov>
2019-10-11 10:59:34 -0400

    Optimizing iso material response...

--- phys.h
+++ phys.h
@@ -929,7 +929,7 @@ static inline void compute_iso_s(FLOAT_PHYS* S, const FLOAT_PHYS* H,
   const FLOAT_PHYS lambda, const FLOAT_PHYS mu, const FLOAT_PHYS dw){
   // S = mu * (H+H^T) + lambda * I * ( H[0]+H[5]+H[10] )
   for(int i=0; i<3; i++){
-    for(int j=0; j<4; j++){ S[4* i+j] = H[4* i+j] * mu * dw; } }
+    for(int j=0; j<4; j++){ S[4* i+j] = H[4* i+j] * mu * dw *2.0; } }
   const FLOAT_PHYS tr = (H[0]+H[5]+H[10]) * lambda * dw;
   S[0]+=tr; S[5]+=tr; S[10]+=tr;
   S[1]+= S[4];
