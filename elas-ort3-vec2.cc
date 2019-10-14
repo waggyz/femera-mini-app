51aa14b2444c2030a2a388b2902b4a6ad201f5e3
David Wagner <david.wagner-1@nasa.gov>
2019-10-14 09:18:12 -0400

    Oops. My AVX2 ortho material eval is slower.

--- elas-ort3-vec2.cc
+++ elas-ort3-vec2.cc
@@ -5,7 +5,7 @@
 #include "femera.h"
 #include <immintrin.h>
 //
-#define USE_AVX2
+#undef USE_AVX2
 //
 int ElastOrtho3D::Setup( Elem* E ){
   JacRot( E );
@@ -139,14 +139,7 @@ int ElastOrtho3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
     {// begin scoping unit
     __m256d vS[3];
 #ifdef USE_AVX2
-    FLOAT_PHYS VECALIGNED H[12], S[8];
-    _mm256_store_pd( &H[0], vH[0] );
-    _mm256_store_pd( &H[4], vH[1] );
-    _mm256_store_pd( &H[8], vH[2] );
-    compute_ort_s_voigt( &S[0], &H[0],&C[0],&vC[0], dw );
-    vS[0]=_mm256_load_pd(&S[0]);
-    vS[1]=_mm256_load_pd(&S[4]);
-    //compute_ort_s_voigt( &vS[0], &vH[0], &vC[0], dw );
+    compute_ort_s_voigt( &vS[0], &vH[0], &vC[0], dw );
     rotate_s_voigt( &vS[0], &vR[0] );// Hmmm
 #else
     FLOAT_PHYS VECALIGNED H[12], S[8];
