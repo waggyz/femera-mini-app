5e6f8bca71f7b49b99b33ca7323132c0b2403bd2
David Wagner <david.wagner-1@nasa.gov>
2019-10-27 06:42:45 -0400

    Trying slice partitioner in profiler...

--- perf/mesh-uhxt.sh
+++ perf/mesh-uhxt.sh
@@ -21,17 +21,22 @@ fi
 #  mv $DIR"/uhxt"$H"p"$P"n"$N".msh2" $DIR"/uhxt"$H"p"$P"n"$N".msh"
 #fi
 if [ ! -f $DIR"/uhxt"$H"p"$P"n1.msh" ]; then
+  mv $DIR"/uhxt"$H"p"$P"n1.msh" $DIR"/uhxt"$H"p"$P"n.msh"
+fi
+if [ ! -f $DIR"/uhxt"$H"p"$P"n.msh" ]; then
   if [ -n "$LOGFILE" ]; then
-    echo "Meshing uhxt"$H"p"$P"n1.msh..." >> "$LOGFILE"
+    echo "Meshing uhxt"$H"p"$P"n.msh..." >> "$LOGFILE"
   else
-    echo "Meshing uhxt"$H"p"$P"n1.msh..."
+    echo "Meshing uhxt"$H"p"$P"n.msh..."
   fi
   gmsh -nt $C -v $VERB -setnumber p $P -setnumber h $H -setnumber n 1 -3 \
-    -format msh2 -o $DIR"/uhxt"$H"p"$P"n1.msh" -save geo/uhxt-cube.geo
+    -format msh2 -o $DIR"/uhxt"$H"p"$P"n.msh" -save geo/uhxt-cube.geo
 fi
 PARTSTR="METIS"
 if [ $N -lt 1000 ]; then
   N=$(( $N / $C * $C ))
+  NN=$N
+  SLICEARG=""
 else
   NXYZ=($(python perf/part_slice_xyz.py -n $N -c $C))
   N=${NXYZ[0]}
@@ -39,22 +44,24 @@ else
   SY=${NXYZ[2]}
   SZ=${NXYZ[3]}
   PARTSTR="slicing "$SX"x"$SY"x"$SZ""
+  NN=""
+  SLICEARG="-xS"$SX" -yS"$SY" -zS"$SZ
 fi
 if [ ! -f $DIR"/uhxt"$H"p"$P"n"$N".msh" ]; then
-  if [ -n "$LOGFILE" ]; then
-    echo "Partitioning to uhxt"$H"p"$P"n"$N".msh ("$PARTSTR")..." >> "$LOGFILE"
-  else
-    echo "Partitioning to uhxt"$H"p"$P"n"$N".msh ("$PARTSTR")..."
-  fi
   if [ $N -lt 1000 ]; then
+    if [ -n "$LOGFILE" ]; then
+      echo "Partitioning to uhxt"$H"p"$P"n"$N".msh ("$PARTSTR")..." >> "$LOGFILE"
+    else
+      echo "Partitioning to uhxt"$H"p"$P"n"$N".msh ("$PARTSTR")..."
+    fi
     gmsh -v $VERB -nt $C -part $N \
       -format msh2 -o $DIR"/uhxt"$H"p"$P"n"$N".msh" \
-      $DIR"/uhxt"$H"p"$P"n1.msh"
-  else
-    gmsh -v $VERB -nt $C -setnumber n $N \
-      -setnumber sx $SX -setnumber sy $SY -setnumber sz $SZ \
-      -format msh2 -o $DIR"/uhxt"$H"p"$P"n"$N".msh" \
-      -merge $DIR"/uhxt"$H"p"$P"n1.msh" -save geo/simplepart.geo
+      $DIR"/uhxt"$H"p"$P"n.msh"
+  #else
+  #  gmsh -v $VERB -nt $C -setnumber n $N \
+  #    -setnumber sx $SX -setnumber sy $SY -setnumber sz $SZ \
+  #    -format msh2 -o $DIR"/uhxt"$H"p"$P"n"$N".msh" \
+  #    -merge $DIR"/uhxt"$H"p"$P"n.msh" -save geo/simplepart.geo
   fi
 fi
 if [ ! -f $DIR"/uhxt"$H"p"$P"n"$N"_1.fmr" ]; then
@@ -66,27 +73,27 @@ if [ ! -f $DIR"/uhxt"$H"p"$P"n"$N"_1.fmr" ]; then
   case $PHYS in
   elas-iso)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-      -M0 -E100e9 -N0.3 -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -a $DIR"/uhxt"$H"p"$P"n"$NN
   ;;
   elas-ort)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-      -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
   ;;
   ther-iso)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 -x@1.0 -Tu10 \
-      -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -a $DIR"/uhxt"$H"p"$P"n"$NN
   ;;
   ther-ort)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 -x@1.0 -Tu10 \
-      -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
   ;;
   plas-iso)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-      -M0 -E100e9 -N0.3 -J300e6 -J100e6 -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -J300e6 -J100e6 -a $DIR"/uhxt"$H"p"$P"n"$NN
   ;;
   *)
     $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-      -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+      $SLICEARG -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
   esac
 else
   # Convert existing .fmr files, if necessary.
@@ -125,27 +132,27 @@ else
     case $PHYS in
     elas-iso)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-        -M0 -E100e9 -N0.3 -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -a $DIR"/uhxt"$H"p"$P"n"$NN
     ;;
     elas-ort)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-        -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
     ;;
     ther-iso)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 -x@1.0 -Tu10 \
-        -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -a $DIR"/uhxt"$H"p"$P"n"$NN
     ;;
     ther-ort)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 -x@1.0 -Tu10 \
-        -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -A20e-6 -K100e-6 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
     ;;
     plas-iso)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-        -M0 -E100e9 -N0.3 -J300e6 -J100e6 -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -J300e6 -J100e6 -a $DIR"/uhxt"$H"p"$P"n"$NN
     ;;
     *)
       $GMSH2FMR -v$VERB -x@0.0 -x0 -y@0.0 -y0 -z@0.0 -z0 -x@1.0 -xu0.001 \
-        -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$N
+        $SLICEARG -M0 -E100e9 -N0.3 -R -a $DIR"/uhxt"$H"p"$P"n"$NN
     esac
   fi
 fi
