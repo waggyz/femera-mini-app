5f632733f9f55c68ac92eb3c51655bfc32eb1240
David Wagner <david.wagner-1@nasa.gov>
2019-12-04 09:54:31 -0500

    Tweaking avx iso driver...

--- elas-iso3-vect.cc
+++ elas-iso3-vect.cc
@@ -7,11 +7,9 @@
 
 // Fetch next u within G,H loop nest
 #undef FETCH_U_EARLY
-#define HAS_AVX
 #ifdef FETCH_JAC
 #define THIS_FETCH_JAC
 #endif
-#undef THIS_FETCH_JAC
 //
 int ElastIso3D::Setup( Elem* E ){
   JacT  ( E );
@@ -47,11 +45,10 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
   printf("DOF: %u, Elems:%u, IntPts:%u, Nodes/elem:%u\n",
     (uint)ndof,(uint)elem_n,(uint)intp_n,(uint)Nc );
 #endif
+#ifdef THIS_FETCH_JAC
   FLOAT_MESH VECALIGNED jac[Nj];
-  FLOAT_PHYS VECALIGNED G[Nt], u[Ne];
-#ifndef HAS_AVX
-  FLOAT_PHYS VECALIGNED H[Nd*4], S[Nd*4];
 #endif
+  FLOAT_PHYS VECALIGNED G[Nt], u[Ne];
   FLOAT_PHYS VECALIGNED intp_shpg[intp_n*Ne];
   FLOAT_PHYS VECALIGNED wgt[intp_n];
   FLOAT_PHYS VECALIGNED matc[this->mtrl_matc.size()];
@@ -81,22 +78,12 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
 //#pragma omp simd
 #endif
     for (int i=0; i<Nc; i++){
-      std::memcpy( & u[Nf*i],&sys_u[c[i]*Nf],sizeof(FLOAT_SOLV)*Nf );}
+      std::memcpy( & u[Nf*i],&sys_u[c[i]*Nf],sizeof(FLOAT_SOLV)*Nf ); }
   }
   for(INT_MESH ie=e0;ie<ee;ie++){//================================== Elem loop
 //#ifndef THIS_FETCH_JAC
 //      std::memcpy( &jac, &Ejacs[Nj*ie], sizeof(FLOAT_MESH)*Nj);
 //#endif
-    const INT_MESH* RESTRICT conn = &Econn[Nc*ie];
-#ifndef HAS_AVX
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#else
-//#pragma omp simd
-#endif
-    for (int i=0; i<Nc; i++){
-      std::memcpy( & f[4*i],& part_f[conn[i]*3], sizeof(FLOAT_SOLV)*Nf ); }
-#endif
 #ifdef THIS_FETCH_JAC
     const __m256d vJ[3]={
       _mm256_load_pd(&jac[0]),   // j0 = [j3 j2 j1 j0]
@@ -109,43 +96,15 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
       _mm256_loadu_pd(&Ejacs[Nj*ie+6]) };// j2 = [j9 j8 j7 j6]
     const FLOAT_MESH det=Ejacs[Nj*ie+9];
 #endif
-  {// Scope vf registers
-  __m256d vf[Nc];
+    const INT_MESH* RESTRICT conn = &Econn[Nc*ie];
+    {// Scope vf registers
+    __m256d vf[Nc];
     for(int ip=0; ip<intp_n; ip++){//============================== Int pt loop
       //G = MatMul3x3xN( jac,shg );
       //H = MatMul3xNx3T( G,u );// [H] Small deformation tensor
-#ifdef HAS_AVX
       __m256d vH[3];
       compute_g_h( &G[0],&vH[0], Ne, &vJ[0], &intp_shpg[ip*Ne], &u[0] );
-#else
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#else
-//#pragma omp simd
-#endif
-      for(int i=0; i<12 ; i++){ H[i]=0.0; };
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#else
-//#pragma omp simd
-#endif
-      for(int i=0; i<Nc; i++){
-        for(int k=0; k<Nf ; k++){ G[4* i+k ]=0.0;
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#endif
-          for(int j=0; j<Nd ; j++){
-            G[(4* i+k) ] += intp_shpg[ip*Ne+ Nd* i+j ] * jac[Nd* j+k ];
-          }
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#endif
-          for(int j=0; j<Nf ; j++){
-            H[(4* k+j) ] += G[(4* i+k) ] * u[Nf* i+j ];
-          }
-        }
-      }//-------------------------------------------------- N*3*6*2 = 36*N FLOP
-#endif
+      //-------------------------------------------------- N*3*6*2 = 36*N FLOP
 #if VERB_MAX>10
       printf( "Small Strains (Elem: %i):", ie );
       for(int j=0;j<H.size();j++){
@@ -173,7 +132,6 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
           std::memcpy( &jac, &Ejacs[Nj*(ie+1)], sizeof(FLOAT_MESH)*Nj );
 #endif
       } }
-#ifdef HAS_AVX
       {// vector register scope
       __m256d vS[3];
       compute_iso_s( &vS[0], &vH[0], C[1]*dw,C[2]*dw );
@@ -186,62 +144,12 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
         } printf("\n");
       }
 #endif
-#else
-      {
-      const FLOAT_PHYS Cdw[3] = { C[0]*dw, C[1]*dw, C[2]*dw };
-      S[ 0]= Cdw[0]* H[0] + Cdw[1]* H[5] + Cdw[1]* H[10];//Sxx
-      S[ 5]= Cdw[1]* H[0] + Cdw[0]* H[5] + Cdw[1]* H[10];//Syy
-      S[10]= Cdw[1]* H[0] + Cdw[1]* H[5] + Cdw[0]* H[10];//Szz
-
-      S[1]=( H[1] + H[4] )*Cdw[2];// S[3]= S[1];//Sxy Syx
-      S[2]=( H[2] + H[8] )*Cdw[2];// S[6]= S[2];//Sxz Szx
-      S[6]=( H[6] + H[9] )*Cdw[2];// S[7]= S[5];//Syz Szy
-      }
-      S[4]=S[1]; S[9]=S[6]; S[8]=S[2];
-#if VERB_MAX>10
-      if(ie==4){
-        printf( "S[%u]:", ie );
-        for(uint j=0;j<12;j++){
-          if(j%4==0){printf("\n");}
-          printf("%+9.2e ",S[j]);
-        } printf("\n");
-      }
-#endif
-#endif
       //------------------------------------------------------- 18+9 = 27 FLOP
-#ifdef HAS_AVX
-    if(ip==0){
-      for(int i=0; i<Nc; i++){ vf[i]=_mm256_loadu_pd(&part_f[3*conn[i]]); }
-    }
-    accumulate_f( &vf[0], &vS[0], &G[0], Nc );
-    }// end vS register scope
-#else
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#else
-#ifdef HAS_PRAGMA_SIMD
-#pragma omp simd
-#endif
-#endif
-      for(int i=0; i<Nc; i++){
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#endif
-        for(int k=0; k<Nf; k++){
-#ifdef __INTEL_COMPILER
-#pragma vector unaligned
-#endif
-          for(int j=0; j<Nf; j++){
-            f[(4* i+k) ] += G[(4* i+j) ] * S[(4* j+k) ];// 18*N FMA FLOP
-      } } }//------------------------------------------------ N*3*6 = 18*N FLOP
-#endif
-#if VERB_MAX>10
-      printf( "f:");
-      for(int j=0;j<Ne;j++){
-        if(j%ndof==0){printf("\n");}
-        printf("%+9.2e ",f[j]);
-      } printf("\n");
-#endif
+      if(ip==0){
+        for(int i=0; i<Nc; i++){ vf[i]=_mm256_loadu_pd(&part_f[3*conn[i]]); }
+      }
+      accumulate_f( &vf[0], &vS[0], &G[0], Nc );
+      }// end vS register scope
     }//========================================================== end intp loop
 #ifdef __INTEL_COMPILER
 #pragma vector unaligned
@@ -254,7 +162,7 @@ int ElastIso3D::ElemLinear( Elem* E, const INT_MESH e0, const INT_MESH ee,
 #endif
       for(int j=0; j<3; j++){
         part_f[3*conn[i]+j] = sf[j]; } }
-  }// end vf register scope
+    }// end vf register scope
   }//============================================================ end elem loop
   return 0;
 }
