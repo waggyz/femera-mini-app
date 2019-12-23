661c7fa1c45acfc7025bfc01d2756ebc337b225f
David Wagner <david.wagner-1@nasa.gov>
2019-12-23 10:28:41 -0500

    Automating intel advisor data collection...

--- perf/femera-advisor-survey-csv.sh
+++ perf/femera-advisor-survey-csv.sh
@@ -31,7 +31,7 @@ for CSTR in icc; do
     PCSV=$PERFDIR"/advixe-"$NAME".csv"
     echo "Extracting "$ADIR" to "$PCSV"..."
     $EXE
-    cut -f 1,7,9,28,49,50,51,52,61,62,64,66,68,70,72 $ACSV > $PCSV
+    cut -f 1,7,9,28,49,50,51,52,61,62,64,66,68,70,72 --output-delimiter=, $ACSV > $PCSV
   fi
 done
 done
