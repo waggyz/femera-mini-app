b6659530e9c4c4ed481ad58d5f817f612d6e557e
David Wagner <david.wagner-1@nasa.gov>
2019-12-23 09:17:12 -0500

    Automating intel advisor data collection...

--- perf/femera-advisor-survey-csv.sh
+++ perf/femera-advisor-survey-csv.sh
@@ -9,19 +9,22 @@ module load intel
 for SIZE in 500kdof 50mdof; do
 for ESTR in tet4 tet10 tet20; do
 for YSTR in iso; do
-#for NSTR in X5675 6148 E5-2640 E5-2650 E5-2670 7210; do
-for NSTR in X5675 6148 E5-2640 E5-2650 E5-2670 7210; do
+#for NSTR in "X5675-6148" X5675 6148 E5-2640 E5-2650 E5-2670 7210; do
+for NSTR in "X5675-6148"; do
 for CSTR in icc; do
-#
-NAME=$SIZE"-"$ESTR"-"$YSTR"-"$NSTR"-"$CSTR
-ADIR=$ADVDIR"/femera-"$NAME
-if [ -d "$DIR" ]; then
-  EXE="advixe-cl --report survey --show-all-columns --no-show-all-rows --format=csv"
-  EXE=$EXE" --project-dir" $ADIR "--report-output=survey-"$NAME".csv"
-  #
-  echo $EXE
-  $EXE
-fi
+  if [ $NSTR=="" ]; then
+    NAME=$SIZE"-"$ESTR"-"$YSTR"-"$NSTR
+  else
+    NAME=$SIZE"-"$ESTR"-"$YSTR"-"$NSTR"-"$CSTR
+  fi
+  ADIR=$ADVDIR"/femera-"$NAME
+  if [ -d "$DIR" ]; then
+    EXE="advixe-cl --report survey --show-all-columns --no-show-all-rows --format=csv"
+    EXE=$EXE" --project-dir" $ADIR "--report-output=survey-"$NAME".csv"
+    #
+    echo $EXE
+    $EXE
+  fi
 done
 done
 done
