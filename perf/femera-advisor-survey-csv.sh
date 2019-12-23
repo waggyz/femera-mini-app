82b50a633f9262b8a1cb0d4220dda5ef165e82a6
David Wagner <david.wagner-1@nasa.gov>
2019-12-23 11:33:48 -0500

    Automating intel advisor data collection...

--- perf/femera-advisor-survey-csv.sh
+++ perf/femera-advisor-survey-csv.sh
@@ -13,8 +13,8 @@ module load intel Python_3.7.1
 for SIZE in 500kdof 50mdof; do
 for ESTR in tet4 tet10 tet20; do
 for YSTR in iso; do
-#for NSTR in "X5675-6148" X5675 6148 E5-2640 E5-2650 E5-2670 7210; do
-for NSTR in "X5675-6148"; do
+for NSTR in "X5675-6148" X5675 6148 E5-2640 E5-2650 E5-2670 7210; do
+#for NSTR in "X5675-6148"; do
 for CSTR in icc; do
   if [ $NSTR=="" ]; then
     NAME=$SIZE"-"$ESTR"-"$YSTR"-"$NSTR
@@ -31,10 +31,9 @@ for CSTR in icc; do
     PCSV=$PERFDIR"/advixe-"$NAME".csv"
     echo "Extracting "$ADIR" to "$PCSV"..."
     $EXE
-    # ID,Total Time,Self Time,Total Elaped Time,Self Elaped Time,
-    # Line,Self GFLOPS,Total GFLOPS,Self AI,Total Arithmetic Intenity,
-    # Self Overall AI,Total Overall AI,Self GFLOP,Self GINTOP,Self Giga OP,
-    # Self Memory GB,Self GB/s
+    # ID,Total Time,Self Time,Total Elapsed Time,Self Elapsed Time,Line,
+    # Self GFLOPS,Total GFLOPS,Self AI,Total Arithmetic Intensity,
+    # Self GFLOP,Total GFLOP,Self Memory GB,Total Memory GB
     cut -f 1,7,9,10,11,28,49,50,51,52,64,65,70,71 --output-delimiter=,\
     $ACSV |  sed  '/[apq]/d' | sed 's/[s<"]//g' | sed '/^$/d' > $PCSV
     #
