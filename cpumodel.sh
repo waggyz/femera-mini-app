84f7d60d9d3c17c7561a41d8f1cfda87e26ac003
David Wagner <david.wagner-1@nasa.gov>
2020-01-09 08:59:51 -0500

    Testing small model intel advisor data collection...

--- cpumodel.sh
+++ cpumodel.sh
@@ -2,4 +2,5 @@
 CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($7)}'`
 if [ "$CPUMODEL" == "CPU" ];then CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($6)}'`;fi
 if [ "$CPUMODEL" == "Phi(TM)" ];then CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($8)}'`;fi
+if [ "$CPUMODEL" == "Processor" ];then CPUMODEL=`grep -m 1 "model name" /proc/cpuinfo | awk '{print($8)}'`;fi
 echo $CPUMODEL
