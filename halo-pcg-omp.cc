d7e341ca8ea5f999db49b2f0fae41df436f71d04
David Wagner <david.wagner-1@nasa.gov>
2020-01-27 14:57:39 -0500

    Checking point-block diagonals...

--- halo-pcg-omp.cc
+++ halo-pcg-omp.cc
@@ -300,14 +300,14 @@ int HaloPCG::Init(){// printf("*** HaloPCG M->Init() ***\n");// Preconditioned C
 #ifdef __INTEL_COMPILER
   const int Nr=3*int(E->elem_conn_n);
   int ik=0;
-  printf("Stiffness:\n);
+  printf("Stiffness:\n");
   for(int i=0;i<Nr;i++){
     for(int j=0;j<=(Nr-i);j++){
       printf( "%6.1e",Y->elem_stiff(ik));
       ik++;
     } printf("\n");
   }
-  printf("Block Diagonal:\n);
+  printf("Block Diagonal:\n");
   for(int i=0;i<int(E->elem_conn_n));i++){
     for(int j=0;j<3;j++){
       printf( "%6.1e",S->part_d(3*i+j));
