a700c2fc6c7d388351dfc951afa75207e869192d
David Wagner <david.wagner-1@nasa.gov>
2020-03-25 10:30:04 -0400

    Generalizing profile-phys.sh...

--- perf/profile-phys.sh
+++ perf/profile-phys.sh
@@ -39,81 +39,110 @@ SX=14; SY=15; SZ=16; N=$(( $SX * $SY * $SZ ));# 3360 parts
 CSV1=$PERFDIR/"50mdof-tet10"
 CSV2=$CPUMODEL"-"$CSTR".csv"
 C=$CPUCOUNT
-P=2; H=121;# 50 MDOF @ 3 DOF/node ---------------------------------------------
-MESHNAME="uhxt"$H"p"$P/"uhxt"$H"p"$P"n"$N
-#
-# plas-xxx Plasticity FIXME not vectorized yet
-#
-PHYS=elas-dmv;# DMAT in global coordinates
-"$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
-echo Warming up...
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
-echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
-  $REPEAT_TEST_N" times..."
-for I in $(seq 1 $REPEAT_TEST_N ); do
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
-    >> $CSV1"-"$PHYS"-"$CSV2
+for P in 2 3 1; do
+#  for PHYS in elas-dmv elas-ort elas-iso ther-iso; do
+  for PHYS in ther-iso; do
+  case $P in
+    1) H=246; ;; # 50 MDOF @ 3 DOF/node ---------------------------------------
+    2) H=121; ;;
+    3) H=80; ;;
+  esac
+  MESHNAME="uhxt"$H"p"$P/"uhxt"$H"p"$P"n"$N
+  case $PHYS in
+    #
+    # plas-xxx Plasticity FIXME not vectorized yet
+    #
+  elas-dmv) # DMAT in global coordinates
+    "$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
+    echo Warming up...
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-"$PHYS"-"$CSV2
+    done
+  ;;
+  elas-ort) # Orthotropic
+    "$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
+    echo Warming up...
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-"$PHYS"-"$CSV2
+    done
+  ;;
+    elas-iso) # Isotropic
+    "$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
+    echo Warming up...
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-"$PHYS"-"$CSV2
+    done
+    #
+    # elas-lms Local matrix storage
+    CSTR=icc
+    CSV2=$CPUMODEL"-"$CSTR".csv"
+    echo Warming up...
+      "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" (elas-lms),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-elas-lms-"$CSV2
+    done
+    CSTR=gcc
+    CSV2=$CPUMODEL"-"$CSTR".csv"
+    ;;
+  ther-iso) # Thermal
+    case $P in
+      1) H=270; ;; # 50 MDOF @ 1 DOF/node ---------------------------------------
+      2) H=178; ;;
+      3) H=119; ;;
+    esac
+    #FIXME Check the node count
+    MESHNAME="uhxt"$H"p"$P/"uhxt"$H"p"$P"n"$N
+    #
+    "$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
+    echo Warming up...
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-"$PHYS"-"$CSV2
+    done
+    # ther-lms Local matrix storage
+    CSTR=icc
+    CSV2=$CPUMODEL"-"$CSTR".csv"
+    echo Warming up...
+      "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
+    echo "Running "$ITERS" iterations of "$MESHNAME" (ther-lms),"\
+      $REPEAT_TEST_N" times..."
+    for I in $(seq 1 $REPEAT_TEST_N ); do
+      "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
+        >> $CSV1"-ther-lms-"$CSV2
+    done
+    CSTR=gcc
+    CSV2=$CPUMODEL"-"$CSTR".csv"
+    #
+    # Return the *.fmr to elas-iso
+    "$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ "elas-iso" "$MESHDIR";
+  ;;
+  #******************************************************************************
+  # thel-iso# thermo-elastic
+  #FIXME not vectorized yet
+  #
+  #P=2; H=112;# 50 MDOF @ 4 DOF/node
+  #
+  # Return the *.fmr to elas-iso
+  #
+  esac
+  done
 done
-#
-PHYS=elas-ort;# Orthotropic
-"$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
-echo Warming up...
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
-echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
-  $REPEAT_TEST_N" times..."
-for I in $(seq 1 $REPEAT_TEST_N ); do
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
-    >> $CSV1"-"$PHYS"-"$CSV2
-done
-#
-PHYS=elas-iso;# Isotropic
-"$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
-echo Warming up...
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
-echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
-  $REPEAT_TEST_N" times..."
-for I in $(seq 1 $REPEAT_TEST_N ); do
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
-    >> $CSV1"-"$PHYS"-"$CSV2
-done
-#
-# elas-lms Local matrix storage
-CSTR=icc
-CSV2=$CPUMODEL"-"$CSTR".csv"
-echo Warming up...
-  "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
-echo "Running "$ITERS" iterations of "$MESHNAME" (elas-lms),"\
-  $REPEAT_TEST_N" times..."
-for I in $(seq 1 $REPEAT_TEST_N ); do
-  "$LMSEXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
-    >> $CSV1"-elas-lms-"$CSV2
-done
-#
-CSTR=gcc
-CSV2=$CPUMODEL"-"$CSTR".csv"
-P=2; H=178;# 50 MDOF @ 1 DOF/node ---------------------------------------------
-MESHNAME="uhxt"$H"p"$P/"uhxt"$H"p"$P"n"$N
-#FIXME Check the node count
-#
-PHYS=ther-iso;# Thermal
-"$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ $PHYS "$MESHDIR";
-echo Warming up...
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS_MIN -p "$MESHDIR"/"$MESHNAME" > /dev/null
-echo "Running "$ITERS" iterations of "$MESHNAME" ($PHYS),"\
-  $REPEAT_TEST_N" times..."
-for I in $(seq 1 $REPEAT_TEST_N ); do
-  "$FMREXE" -v1 -c$C -r$RTOL -i$ITERS -p "$MESHDIR"/"$MESHNAME" \
-    >> $CSV1"-"$PHYS"-"$CSV2
-done
-#
-# Return the *.fmr to elas-iso
-"$PERFDIR"/"mesh-part.sh" $H $P $SX $SY $SZ "elas-iso" "$MESHDIR";
-#
-#******************************************************************************
-# thel-iso# thermo-elastic
-#FIXME not vectorized yet
-#
-#P=2; H=112;# 50 MDOF @ 4 DOF/node
-#
-# Return the *.fmr to elas-iso
 #
