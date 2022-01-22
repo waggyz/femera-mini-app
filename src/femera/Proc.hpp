3f67f77ee7579943d33ffa54d35685b3635ae1bc
David Wagner <david.wagner-12nasa.gov>
2022-01-22 08:14:33 -0500

    clean and tidy...

--- src/femera/Proc.hpp
+++ src/femera/Proc.hpp
@@ -11,47 +11,36 @@ namespace femera {
   class Proc : public Work {
   private:
     using This_t = std::shared_ptr<T>;
-  protected:// Set by derived instances during task_init()...
+  protected:                   // Set by derived instances during task_init()...
     proc::Team_t   team_id = 0;
     fmr::Local_int team_n  = 1;
-    fmr::Local_int main_ix = 0;
+    fmr::Local_int proc_id = 0;// global thread identifier
+    fmr::Local_int proc_ix = 0;// index at this level [0,proc_n-1]
     fmr::Local_int proc_n  = 1;
-    fmr::Local_int proc_ix = 0;
-  public:   // ...because proc::Main uses these.
-    bool is_main ();
+    fmr::Local_int main_ix = 0;
+  public:                      //              ...because proc::Main uses these.
+    bool            is_main    ();
     proc::Team_t   get_team_id () noexcept;
     fmr::Local_int get_team_n  () noexcept;
     fmr::Local_int get_proc_ix () noexcept;
     fmr::Local_int get_proc_n  () noexcept;
     //
-#if 0
-    static constexpr fmr::Local_int get_node_n  ();
-    static constexpr fmr::Local_int get_node_ix ();
-#endif
-#if 0
-    constexpr
-    static  fmr::Local_int get_node_core_n  ();
-    static  fmr::Local_int get_node_hype_n  ();
-    static  fmr::Local_int get_node_numa_n  ();
-    static  fmr::Local_int get_node_core_ix ();
-    static  fmr::Local_int get_node_hype_ix ();
-    static  fmr::Local_int get_node_numa_ix ();
-    static fmr::Global_int get_used_byte    ();
-#endif
-    //
     fmr::Exit_int init (int*, char**)        noexcept final override;
     fmr::Exit_int exit (fmr::Exit_int err=0) noexcept final override;
     //
-    This_t                  get_task_spt (fmr::Local_int)         ;
-    This_t                  get_task_spt (Work::Task_path_t)      ;
-    T*                      get_task_raw (fmr::Local_int)         ;
-    T*                      get_task_raw (Work::Task_path_t)      ;
-    static constexpr This_t new_task ()                   noexcept;
-    static constexpr This_t new_task (const Work::Core_t) noexcept;
+    static constexpr
+    This_t new_task     () noexcept;
+    static constexpr
+    This_t new_task     (const Work::Core_t) noexcept;
+    T*     get_task_raw (fmr::Local_int);
+    T*     get_task_raw (Work::Task_path_t);
   private:
-    T*       derived   (Proc*) noexcept;
-    T*       derived   (Work*) noexcept;
-    Proc<T>* this_cast (Work*) noexcept;
+    This_t get_task_spt (fmr::Local_int);
+    This_t get_task_spt (Work::Task_path_t);
+  private:
+    T*          derived (Proc*) noexcept;
+    T*          derived (Work*) noexcept;
+    Proc<T>*  this_cast (Work*) noexcept;
   protected:// Make it clear this class needs to be inherited from.
     Proc ()            =default;
     Proc (const Proc&) =default;
