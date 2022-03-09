0bd1d2a963d1ff54d1791353137955e4bf58866e
David Wagner <david.wagner-12nasa.gov>
2022-03-09 08:12:55 -0500

    clean and tidy...

--- src/femera/data/Cgns.cpp
+++ src/femera/data/Cgns.cpp
@@ -13,6 +13,7 @@ namespace femera {
   void data::Cgns::task_init (int*, char**) {
     this->version = std::to_string (CGNS_VERSION / 1000)
       + "." + std::to_string (CGNS_VERSION % 1000);
+#ifdef FMR_HAS_MPI
     FMR_PRAGMA_OMP(omp master)
     if (this->proc != nullptr) {
       const auto P = this->proc->get_task (Plug_type::Fmpi);
@@ -21,15 +22,18 @@ namespace femera {
         const auto err = MPI_Comm_dup (MPI_Comm (P->get_team_id()), &c);
         if (!err) {
           this->team_id = proc::Team_t (c);
+          this->version+=" (parallel)";
         }
 #if 0
         if(this->proc->log->detail > 1 ){
           this->proc->log->label_printf("CGNS mode",
-            "Parallel access using %i MPI threads/team\n",//TODO
+            "Parallel access using %i MPI threads/team\n",//TODO?
             this->proc->task.first<Proc>( Plug_type:: Pmpi )->get_proc_n() );
         }
 #endif
-  } } }
+    } }
+#endif
+  }
   void data::Cgns::task_exit () {
 //TODO    this->close_all ();
 //TODO?   FMR_PRAGMA_OMP(omp barrier)
