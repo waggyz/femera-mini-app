e294138ed5a9060bf2d2923320b08d164b82b258
David Wagner <david.wagner-12nasa.gov>
2022-04-16 09:51:26 -0400

    started working on Vals::bulk_ints...

--- src/femera/data/Vals.hpp
+++ src/femera/data/Vals.hpp
@@ -3,11 +3,42 @@
 
 #include "../Data.hpp"
 
+#include <unordered_map>
+#include <immintrin.h>       // __m256d
+
 namespace femera { namespace data {
   class Vals;//TODO don't derive CRTP concrete class from Data; Data has vtable
   class Vals final: public Data <Vals> { private: friend class Data;
   private:
+    struct alignas (size_t) Bulk_ints {
+      std::vector <fmr::Bulk_int> bulk_ints;
+    };
+    struct alignas (__m256d) Bulk_vals {
+      std::vector <fmr::Bulk_int> bulk_vals;
+    };
     proc::Team_t team_id = 0;// MPI communicator
+    std::unordered_map <Vals_id, Bulk_ints> name_ints = {};
+    std::unordered_map <Vals_id, Bulk_vals> name_vals = {};
+  public:
+#if 1
+  template <typename I>
+  I* add_get (Vals_id& id,//NOTE includes char type
+    typename std::enable_if<std::is_integral<I>::value>::type* = 0)
+  noexcept {
+    if (true) {//TODO check if stored as I
+      return reinterpret_cast<I*> (name_ints.at(id).bulk_ints.data());
+    } else {//TODO convert to I and store back
+      std::vector<I> bulk_ints(0);
+      bulk_ints.reserve(name_ints.at(id).bulk_ints.capacity());
+      return bulk_ints.data();
+  } }//TODO move to Vals.ipp
+  template <typename I>
+  I* get (Vals_id& id,//NOTE includes char type
+    typename std::enable_if<std::is_integral<I>::value>::type* = 0) {
+    return reinterpret_cast<I*> (name_ints.at(id).bulk_ints.data());
+    //TODO catch std::out_of_range
+  }//TODO move to Vals.ipp
+#endif
   private:
     void task_init (int* argc, char** argv);
     void task_exit ();
