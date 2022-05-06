32284eb1aa3fb8d63e2e7ed4cb27527e04b0d6ee
David Wagner <david.wagner-12nasa.gov>
2022-05-06 08:08:58 -0400

    clean and tidy...

--- src/zyclops/Zomplex.hpp
+++ src/zyclops/Zomplex.hpp
@@ -2,7 +2,6 @@
 #define ZYC_HAS_ZOMPLEX_HPP
 
 #include "zyclops.hpp"
-#include "data.hpp"
 
 namespace zyc {
 
@@ -10,7 +9,7 @@ namespace zyc {
   private:
     Algebra           family = Algebra::Real;
     Zorder_int         order = 0;
-    zyc::data::Layout layout = zyc::data::Layout::Native;
+    zyc::Layout layout = zyc::Layout::Native;
     bool        is_transpose = false;
     bool           has_upper = false;// triangular or full matrix storage
     bool           has_lower = false;// triangular or full matrix storage
@@ -25,26 +24,26 @@ namespace zyc {
     bool       is_sparse    (){ return ! has_zeros; }
     Algebra    get_family        (){ return this->family; }
     Zorder_int get_order         (){ return this->order; }
-    zyc::data::Layout get_layout (){ return this->layout; }
+    zyc::Layout get_layout (){ return this->layout; }
   public:
     Zomplex () {}
     Zomplex (Algebra z, Zorder_int p) : family (z), order(p) {
-      if (p!=0) {layout = zyc::data::Layout::Block;}
+      if (p!=0) {layout = zyc::Layout::Block;}
     }
-    Zomplex (Algebra z, Zorder_int p, zyc::data::Layout l) :
+    Zomplex (Algebra z, Zorder_int p, zyc::Layout l) :
       family (z), order(p), layout(l) {}
   };
   const static Zomplex Real
-    = Zomplex (Algebra::Real, 0, zyc::data::Layout::Native);
+    = Zomplex (Algebra::Real, 0, zyc::Layout::Native);
   const static Zomplex Native_complex
-    = Zomplex (Algebra::Complex, 1, zyc::data::Layout::Native);
+    = Zomplex (Algebra::Complex, 1, zyc::Layout::Native);
   const static Zomplex Unit_quaternion
-    = Zomplex (Algebra::Quat, 2, zyc::data::Layout::Vector);
+    = Zomplex (Algebra::Quat, 2, zyc::Layout::Vector);
   //
   const static Zomplex Integer
-    = Zomplex (Algebra::Int, 0, zyc::data::Layout::Native);
+    = Zomplex (Algebra::Int, 0, zyc::Layout::Native);
   const static Zomplex Natural
-    = Zomplex (Algebra::Nat, 0, zyc::data::Layout::Native);
+    = Zomplex (Algebra::Nat, 0, zyc::Layout::Native);
 
 }// end zyc:: namespace
 
--- src/zyclops/zyclops.hpp
+++ src/zyclops/zyclops.hpp
@@ -3,22 +3,29 @@
 
 #include <cstdint>
 
+#define ZYC_ALIGN_INTS alignof(size_t)
+#define ZYC_ALIGN_VALS alignof(__m256d)
+
+#ifndef ZYC_RESTRICT
+#define ZYC_RESTRICT __restrict
+#endif
+#define ZYC_ARRAY_PTR auto* ZYC_RESTRICT
+#define ZYC_CONST_PTR const auto* ZYC_RESTRICT
+#define ZYC_ALIGN_PTR __attribute__((aligned(ZYC_ALIGN_VALS))) auto* ZYC_RESTRICT
+
 namespace zyc {
 
   using Zorder_int = uint8_t;
-#ifdef FMR_HAS_ZYCLOPS
-  enum class Algebra     : int8_t {Unknown =0,
-    Real=zyc::Real, Complex=zyc::Complex, Dual=zyc::Dual, Split=zyc::Split,
-    Quat=zyc::Quat,     OTI=zyc::OTI    , User=zyc::User,
-    Int, Nat
-  };
-#else
   enum class Algebra     : int8_t {Unknown =0,
     Real, Complex, Dual, Split, Quat, OTI, User,
     Int,// signed
     Nat // unsigned
   };
-#endif
+  enum class Layout      : int8_t { Unknown =0,// for arrays of Zomplex numbers
+    Vector=1,// AoS (array of structs, interleaved real & imaginary parts)
+    Block =2,// SoA (struct of arrays, arrays of real & each imaginary part)
+    Native=3 // Native is for real & built-in complex type
+  };
 }// end zyc namespace
 
 //end ZYC_HAS_ZYCLOPS_HPP
