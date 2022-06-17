344c239e925f862db94243533b111dd3a9c037a7
David Wagner <david.wagner-12nasa.gov>
2022-06-17 08:26:48 -0400

    clean and tidy...

--- src/femera/test/Self.cpp
+++ src/femera/test/Self.cpp
@@ -15,17 +15,20 @@ namespace femera {
     const auto all_n = this->proc->all_proc_n ();
     const auto node_n = this->proc->get_task (Task_type::Node)->get_team_n ();
     const auto core_n = node_n * proc::Node::get_core_n ();
+    const auto str = get_base_abrv ()+" "+ abrv +" ";
     if (true) {//TODO detail && this->test->do_test
+      this->data->name_line (data->fmrlog, str +"zord",
+        "%4u max      hypercomplex order", zyclops::max_zorder);
       fmr::Local_int mpi_n=0, omp_n=0;
       const auto Pmpi = this->proc->get_task (Task_type::Fmpi);
       if (Pmpi) {mpi_n = Pmpi->get_proc_n ();}
       const auto Pomp = this->proc->get_task (Task_type::Fomp);
       if (Pomp) {omp_n = Pomp->get_proc_n ();}
-      this->data->name_line (data->fmrlog, get_base_abrv ()+" "+ abrv +" proc",
-        "%4u MPI %4u OpenMP = %4u of %4u total CPU processes",
+      this->data->name_line (data->fmrlog, str +"proc",
+        "%4u MPI %4u OpenMP : %4u of %4u total CPU processes",
         mpi_n, omp_n, mpi_n * omp_n,  all_n);
-      this->data->name_line (data->fmrlog, get_base_abrv ()+" "+ abrv +" node",
-        "%4u node%4u cores  = %4u of %4u total physical cores",
+      this->data->name_line (data->fmrlog, str +"node",
+        "%4u node%4u cores  : %4u of %4u total physical cores",
         node_n, core_n, node_n * core_n, all_n);
     }
     if (all_n < core_n) {
@@ -37,8 +40,5 @@ namespace femera {
       this->data->name_line (data->fmrlog, this->abrv+" task_init",
         "NOTE Femera uses more threads (%u) than physical cores (%u).",
         all_n, core_n);
-  }
-  this->data->name_line (data->fmrlog, get_base_abrv ()+" "+ abrv +" zord",
-    "%4u maximum hypercomplex order", zyclops::max_zorder);
-  }
+  } }
 }//end femera namespace
