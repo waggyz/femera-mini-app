e9beb206d54af6d27e731cf64c3ac05245b6279b
David Wagner <david.wagner-12nasa.gov>
2022-07-06 05:58:39 -0400

    clean and tidy...

--- src/femera/test/Self.cpp
+++ src/femera/test/Self.cpp
@@ -1,7 +1,7 @@
 #include "../core.h"
 #include "Self.hpp"
 
-//#include "../proc/Node.hpp"//TODO Remove.
+#include "../proc/Node.hpp"//TODO Remove?
 
 #include <valarray>
 
@@ -16,6 +16,7 @@ namespace femera {
     if (true) {//TODO detail && this->test->do_test
       this->log_init_info ();// pulled out method to prevent inline fail warning
       //TODO call proc->chck (Task_type::Node);
+      this->proc_node_chck ();
     }
     return;
   }
@@ -36,5 +37,65 @@ FMR_WARN_INLINE_OFF
     this->data->name_line (data->fmrlog, str +"zord",
       "%4u maximum hypercomplex order", zyclops::max_zorder);
 FMR_WARN_INLINE_ON
+  }
+  fmr::Exit_int test::Self::proc_node_chck () {//TODO interface in File and Proc classes
+    fmr::Exit_int err = 0;
+    const auto all_n = this->proc->all_proc_n ();
+    const auto node_n = this->proc->get_task (Task_type::Node)->get_team_n ();
+    const auto core_n = node_n * proc::Node::get_core_n ();
+    const auto numa_n = node_n * proc::Node::get_numa_n ();
+    const auto numa_all = node_n * numa_n;
+FMR_WARN_INLINE_OFF
+    if (true) {//TODO detail && this->test->do_test
+      const auto str = this->get_base_abrv ()+" "+ this->abrv +" ";
+      fmr::Local_int mpi_n=0, omp_n=0;
+      //
+      // segfaults here if called before tasks init
+      const auto Pmpi = this->proc->get_task (Task_type::Fmpi);
+      if (Pmpi) {mpi_n = Pmpi->get_proc_n ();}
+      const auto Pomp = this->proc->get_task (Task_type::Fomp);
+      if (Pomp) {omp_n = Pomp->get_proc_n ();}
+#if 0
+      this->data->name_line (data->fmrlog, str +"proc",
+        "%4u MPI %4u OpenMP: %4u of %4u total CPU processes",
+        mpi_n, omp_n, mpi_n * omp_n,  all_n);
+      this->data->name_line (data->fmrlog, str +"node",
+        "%4u node%4u cores : %4u of %4u total physical cores",
+        node_n, core_n, node_n * core_n, all_n);
+#else
+#ifdef FMR_TIMES_UCHAR
+      auto x = std::string ( MAKESTR(\FMR_TIMES_UCHAR) );
+      x = (x =="\\x") ? "x" : x ;
+#else
+      const auto x = std::string ("x");
+#endif
+      this->data->name_line (data->fmrlog, str +"proc",
+//      printf (
+        ("%4u    /%4u total CPU processes (%4u MPI  "+ x +"%4u OpenMP)").c_str(),
+        mpi_n * omp_n, all_n, mpi_n, omp_n);
+      this->data->name_line (data->fmrlog, str +"node",
+//      printf (
+        ("%4u    /%4u total physical CPUs (%4u node "+ x +"%4u cores)").c_str(),
+        node_n * core_n, all_n, node_n, core_n);
+      this->data->name_line (data->fmrlog, str +"numa",
+//      printf (
+        ("%4u ea "+ x +"%4u node:%4u NUMA domain%s total").c_str(),
+        numa_n, node_n, numa_all, (numa_all==1)?"":"s");
+#endif
+    }
+    if (all_n < core_n) { err =-1;
+      this->data->name_line (data->fmrlog, this->abrv+" task_init",
+//      printf (
+        "NOTE Femera uses fewer threads (%u) than physical cores (%u).",
+        all_n, core_n);
+    }
+    if (all_n > core_n) { err =-1;
+      this->data->name_line (data->fmrlog, this->abrv+" task_init",
+//      printf (
+        "NOTE Femera uses more threads (%u) than physical cores (%u).",
+        all_n, core_n);
+    }
+FMR_WARN_INLINE_ON
+  return err;
   }
 }//end femera namespace
