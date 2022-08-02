f411f640be72de0063240c7d07a4f10a144567ff
David Wagner <david.wagner-12nasa.gov>
2022-08-02 07:36:06 -0400

    working on custom GoogleTest listeners...

--- src/femera/test/Gtst.cpp
+++ src/femera/test/Gtst.cpp
@@ -13,8 +13,32 @@
 #if 1
 namespace femera { namespace test { namespace gtst {
 
+  class MinimalistPrinter : public ::testing::EmptyTestEventListener {
+    /* from: https://github.com/google/googletest/blob/main/docs/advanced.md
+     */
+    // Called before a test starts.
+    void OnTestStart(const ::testing::TestInfo& test_info) override {
+      printf("test gtst  run %s.%s starting.\n",
+        test_info.test_suite_name(), test_info.name());
+    }
+    // Called after a failed assertion or a SUCCESS().
+    void OnTestPartResult(const ::testing::TestPartResult& test_part_result)
+    override {//NOTE only reports failures
+      printf("test gtst %4s in %s:%d\n%s\n",
+        test_part_result.failed() ? "FAIL" : "ok",
+        test_part_result.file_name(),
+        test_part_result.line_number(),
+        test_part_result.summary());
+    }
+    // Called after a test ends.
+    void OnTestEnd(const ::testing::TestInfo& test_info) override {
+      printf("test gtst  run %s.%s ending.\n",
+        test_info.test_suite_name(), test_info.name());
+    }
+  };
+
 TEST( MiniGtest, RunsGtests ){
-  EXPECT_EQ( 0, 0 );
+  ASSERT_EQ( 1, 1 );
 }
 
 } } }//end femera::test::gtst:: namespace
@@ -53,12 +77,19 @@ namespace femera {
   void test::Gtst::task_init (int* argc, char** argv) {
     this->scan (argc, argv);
 #ifdef FMR_HAS_MPI
-    //from: https://github.com/google/googletest/issues/822
+    /*from: https://github.com/google/googletest/issues/822
+     * and: https://github.com/google/googletest/blob/main/docs/advanced.md
+     */
     ::testing::TestEventListeners& listeners
       = ::testing::UnitTest::GetInstance()->listeners();
-    if ( !this->is_enabled || !this->proc->is_main()) {// Only report from main
+    if ( !this->is_enabled || !this->proc->is_main()) {
+      // Remove default listener from all except main
       delete listeners.Release (listeners.default_result_printer());
+      //TODO Remove all default listeners and replace with custom listeners.
+      //     Print from main thread only, and log all threads to file.
     }
+    // Add custom listener to all
+    listeners.Append(new test::gtst::MinimalistPrinter);
 #endif
   this->set_init (true);
   }
