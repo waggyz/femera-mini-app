f95ddc728383c8dd994efc4fd0cdd1db521c5e00
David Wagner <david.wagner-1@nasa.gov>
2025-03-10 19:39:07 -0400

    clean and tidy...

--- src/pymera/test/uq_straw_1a.py
+++ src/pymera/test/uq_straw_1a.py
@@ -10,17 +10,22 @@ import numpy as np
 
 N = 1000 # number of simulations
 
-# Set up random variables as size N numpy arrays.
-tip_displacement_z = np.random.normal(0.100, 0.010, N) # mean=0.100, stdev=0.010
-length = np.random.normal(1.000, 0.010, N)
-width = np.random.normal(0.050, 0.005, N)
-height = np.random.normal(0.050, 0.005, N)
-youngs = np.random.normal(210e9, 210e8, N)
-poissons = np.random.normal(0.285, 0.0285, N)
+# Set up random input variables as size N numpy arrays.
+tip_displacement_z = np.ascontiguousarray(#TODO hide ascontiguousarray.
+    np.random.normal(0.100, 0.010, N)) # mean=0.100, stdev=0.010
+length = np.ascontiguousarray(
+    np.random.normal(1.000, 0.010, N))
+width = np.ascontiguousarray(
+    np.random.normal(0.050, 0.005, N))
+height = np.ascontiguousarray(
+    np.random.normal(0.050, 0.005, N))
+youngs = np.ascontiguousarray(
+    np.random.normal(210e9, 210e8, N))
+poissons = np.ascontiguousarray(
+    np.random.normal(0.285, 0.0285, N))
 
 jobs = pym.jobs()
-sims = jobs.add_sims(name='cantilever-beam-sims')
-sims.set_runs_n(N)
+sims = jobs.add_sims(name='cantilever-beam-sims', runs_n=N)
 
 sims.set_geometry(name='fmr:geom:block')# global geometry
 sims.set_bcs(name='fixed-base-bc',
@@ -28,7 +33,7 @@ sims.set_bcs(name='fixed-base-bc',
              set='fmr:phys:node:displacement:xyz', to='fmr:phys:bcs:encastre')
 sims.add_bcs(name='tip-z-displacement-bc',
              at='fmr:geom:plane:x-max',
-             set='fmr:phys:node:displacement:z', to='tip-displacement-z') 
+             set='fmr:phys:node:displacement:z', to='tip-displacement-z')
 
 sims.set_material(name='fmr:mtrl:steel',
                   physics='fmr:mtrl:linear-elastic-isotropic')
@@ -40,15 +45,14 @@ sims.set_solve(name='linear-solve', method='fmr:solve:PCG')
 sims.set_grid(method='fmr:grid:FE', elem='fmr:elem:tet10', elem_size=0.020)
 sims.set_partition_n(1)# 1 partition per model
 
-#TODO is 'mean' a synonym for 'to'?
 sims.set_parameter(name='tip-displacement-z', to=tip_displacement_z)
-sims.set_parameter(name='fmr:geom:length', to=length) #x (global parameter)
+sims.set_parameter(name='fmr:geom:length', to=length) #x
 sims.set_parameter('fmr:geom:width', width)# y
 sims.set_parameter('fmr:geom:height', height)# z
 sims.set_parameter('fmr:mtrl:youngs-modulus', youngs)# E
-sims.set_parameter('fmr:mtrl:poissons-ratio', poissons)# nu  
+sims.set_parameter('fmr:mtrl:poissons-ratio', poissons)# nu
 
-# Identify parameters for post-processing
+# Identify parameters for post-processing.
 sims.add_post(name='base-force-mag',
               at='fmr:geom:plane:x-min',
               sum='fmr:phys:node:force:mag')
@@ -56,7 +60,7 @@ sims.add_post(name='base-force-mag',
 sims.init()
 sims.run()
 
-# post-process numpy arrays from Pymera
+# Get numpy array contents from Pymera.
 base_force = sims.get_post('base-force-mag')
 base_pressure_avg = base_force / (width * height)
 
--- src/pymera/test/uq_straw_1b.py
+++ src/pymera/test/uq_straw_1b.py
@@ -10,8 +10,7 @@ import uqtools as uq
 N = 1000 # number of simulations
 
 jobs = pym.jobs()
-sims = jobs.add_sims(name='cantilever-beam-sims')
-sims.set_runs_n(N)
+sims = jobs.add_sims(name='cantilever-beam-sims', runs_n=N)
 
 sims.set_geometry(name='fmr:geom:block')# global geometry
 sims.set_bcs(name='fixed-base-bc',
@@ -19,7 +18,7 @@ sims.set_bcs(name='fixed-base-bc',
              set='fmr:phys:node:displacement:xyz', to='fmr:phys:bcs:encastre')
 sims.add_bcs(name='tip-z-displacement-bc',
              at='fmr:geom:plane:x-max',
-             set='fmr:phys:node:displacement:z', to='tip-displacement-z') 
+             set='fmr:phys:node:displacement:z', to='tip-displacement-z')
 
 sims.set_material(name='fmr:mtrl:steel',
                   physics='fmr:mtrl:linear-elastic-isotropic')
@@ -34,13 +33,13 @@ sims.set_partition_n(1)# 1 partition per model
 # Set up random variables. These will all be size N arrays.
 #TODO is 'mean' a synonym for 'to'?
 sims.set_parameter(name='tip-displacement-z', mean=0.10, stdev=0.01)
-sims.set_parameter(name='fmr:geom:length', to=1.000) #x (global parameter)
+sims.set_parameter(name='fmr:geom:length', to=1.000) #x
 sims.set_parameter('fmr:geom:width',  0.050)# y
 sims.set_parameter('fmr:geom:height', 0.050)# z
 sims.set_parameter('fmr:mtrl:youngs-modulus', 210e9)# E
 sims.set_parameter('fmr:mtrl:poissons-ratio', 0.30)# nu  
 
-# Identify parameters for post-processing
+# Identify parameters for post-processing.
 sims.add_post('fmr:geom:parameters')# all geometry parameters set
 sims.add_post('fmr:mtrl:parameters')# all material parameters set
 sims.add_post('tip-displacement')
@@ -51,7 +50,7 @@ sims.add_post(name='base-force-mag',
 sims.init()
 sims.run()
 
-# post-process numpy arrays from Pymera
+# post-process numpy arrays from Pymera.
 base_force = sims.get_post('base-force-mag')
 base_area = ( sims.get_parameter('fmr:geom:width')
             * sims.get_parameter('fmr:geom:height') )
