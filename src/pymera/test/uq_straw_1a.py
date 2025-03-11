62d0a22b2037a07ced95d33bd0d99fb422c362c2
David Wagner <david.wagner-1@nasa.gov>
2025-03-11 19:38:50 -0400

    tweaking uq_straw_1a.py

--- src/pymera/test/uq_straw_1a.py
+++ src/pymera/test/uq_straw_1a.py
@@ -16,11 +16,11 @@ def main():
     elem_size = 0.010
     
     nominal_dims = np.array([beam_length, beam_width, beam_height])
-    beam_elem_dims = np.ascontiguousarray(
+    beam_elem_count = np.ascontiguousarray(
         np.array(nominal_dims / elem_size, dtype='u4'))
     
     # Set up random input variables as size N numpy arrays.
-    tip_displacement_z = np.ascontiguousarray(#TODO hide ascontiguousarray.
+    tip_z = np.ascontiguousarray(#TODO hide ascontiguousarray.
         np.random.normal(0.100, 0.010, N)) # mean=0.100, stdev=0.010
     length = np.ascontiguousarray(
         np.random.normal(beam_length, beam_length/10, N))
@@ -33,16 +33,19 @@ def main():
     poissons = np.ascontiguousarray(
         np.random.normal(0.285, 0.0285, N))
     
-    my_jobs = pym.jobs()
+    my_jobs = fmr.jobs()
     sims = my_jobs.add_sims(name='cantilever-beam-sims', runs_n=N)
     
-    sims.set_geometry(name='fmr:geom:block')# global geometry
+    sims.add_geometry(name='beam-geometry', shape='fmr:geom:block')
+    sims.add_grid(name='beam-mesh', method='fmr:grid:FE', structured=true,
+                 elem='fmr:elem:tet10')
+
     sims.set_bcs(name='fixed-base-bc',
                 at='fmr:grid:node:x-min',
-                set='fmr:phys:node:displacement:xyz',to='fmr:phys:bcs:encastre')
-    sims.add_bcs(name='tip-z-displacement-bc',
-                at='fmr:grid:node:x-max',
-                set='fmr:phys:node:displacement:z', to='tip-displacement-z')
+                set='fmr:phys:node:displacement:xyz',
+                to='fmr:phys:bcs:encastre')
+    sims.add_bcs(name='tip-bc',
+                at='fmr:grid:node:x-max')# value is tip-bc parameter below
     
     sims.set_material(name='basic-steel',
                     physics='fmr:mtrl:linear-elastic-isotropic')
@@ -51,17 +54,15 @@ def main():
     sims.set_solve(name='linear-solve', method='fmr:solve:PCG')
     # defaults: analysis='fmr:solve:static', load_step_n=1, rtol=1e-6)
     
-    sims.set_grid(method='fmr:grid:FE', structured=true, elem='fmr:elem:tet10')#,
-    #            elem_dims=beam_elem_dims)
     sims.set_partition_n(1)# 1 partition per model
-    sims.set_parameter(name='fmr:grid:cell:dims', to=beam_elem_dims) #x
-    sims.set_parameter('fmr:geom:block:width', width)# y
-    sims.set_parameter(name='tip-displacement-z', to=tip_displacement_z)
-    sims.set_parameter(name='fmr:geom:block:length', to=length) #x
-    sims.set_parameter('fmr:geom:block:width', width)# y
-    sims.set_parameter('fmr:geom:block:height', height)# z
-    sims.set_parameter('fmr:mtrl:youngs-modulus', youngs)# E
-    sims.set_parameter('fmr:mtrl:poissons-ratio', poissons)# nu
+
+    sims.set_parameter('beam-mesh','fmr:grid:cell_count_lwh', beam_elem_count)
+    sims.set_parameter('tip-bc', 'fmr:phys:node:displacement:z', tip_z)
+    sims.set_parameter('beam-geometry', 'fmr:geom:length', length) #x
+    sims.set_parameter('beam-geometry', 'fmr:geom:width', width)# y
+    sims.set_parameter('beam-geometry', 'fmr:geom:height', height)# z
+    sims.set_parameter('basic-steel', 'fmr:mtrl:youngs-modulus', youngs)# E
+    sims.set_parameter('basic-steel', 'fmr:mtrl:poissons-ratio', poissons)# nu
     
     # Identify parameters for post-processing.
     sims.add_post(name='base-force-mag',
